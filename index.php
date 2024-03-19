e82f218fb242024c85fcdc054be284b2bcd4cc27
Augusto Madeira Splett <augusto.splett@gmail.com>
2024-03-19 12:45:30 -0400

    Created the first propouse for homepage, added some styles

--- index.php
+++ index.php
@@ -4,12 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </head>
 <body>
-    <img src="public\img\landing-page-banner.jpg" class="img-fluid" alt="landing page banner">
-    <button type="button" class="btn btn-success">Login</button>
-    <button type="button" class="btn btn-success">SignUp</button>
+    <div class="my-container">
+        <div class="glass-container">
+            <h1>Welcome to Kid's Game!</h1>
+            <div class="buttons-holder">
+                <button type="button" class="btn btn-success">Login</button>
+                <button type="button" class="btn btn-success">SignUp</button>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
 
