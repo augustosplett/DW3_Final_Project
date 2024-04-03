7eba54f9dd55f87af01c5dddebb9cb0fa3629b39
Juan Manuel <127610369+HalcyonDeveloper@users.noreply.github.com>
2024-04-03 15:36:20 -0400

    Update index.php

--- src/PasswordManagement/index.php
+++ src/PasswordManagement/index.php
@@ -5,30 +5,69 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password Change</title>
     <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $("#password_change_form").submit(function(event) {
+                event.preventDefault(); // Evita el envío del formulario por defecto - Juan Pinero
+                var username = $("#username").val();
+                var firstName = $("#first_name").val();
+                var lastName = $("#last_name").val();
+                var newPassword = $("#new_password").val();
+                var confirmPassword = $("#confirm_password").val();
+                
+                if (newPassword !== confirmPassword) {
+                    alert('Passwords do not match');
+                    return;
+                }
+
+                // Envío de los datos - Juan Pinero
+                $.ajax({
+                    type: "POST",
+                    url: "password_change_submit.php",
+                    data: {
+                        username: username,
+                        first_name: firstName,
+                        last_name: lastName,
+                        new_password: newPassword
+                    },
+                    success: function(response){
+                        alert(response); 
+                    }
+                });
+            });
+        });
+    </script>
 </head>
 <body>
     <div id="container">
         <h2>Password Change</h2>
-        <form action="password_change_submit.html" method="post">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required>
+        <form id="password_change_form">
+            <fieldset>
+                <legend>User Information</legend>
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required>
 
-            <label for="first_name">First Name:</label>
-            <input type="text" id="first_name" name="first_name" required>
+                <label for="first_name">First Name:</label>
+                <input type="text" id="first_name" name="first_name" required>
 
-            <label for="last_name">Last Name:</label>
-            <input type="text" id="last_name" name="last_name" required>
+                <label for="last_name">Last Name:</label>
+                <input type="text" id="last_name" name="last_name" required>
+            </fieldset>
 
-            <label for="new_password">New Password:</label>
-            <input type="password" id="new_password" name="new_password" required>
+            <fieldset>
+                <legend>New Password</legend>
+                <label for="new_password">New Password:</label>
+                <input type="password" id="new_password" name="new_password" required>
 
-            <label for="confirm_password">Confirm Password:</label>
-            <input type="password" id="confirm_password" name="confirm_password" required>
+                <label for="confirm_password">Confirm Password:</label>
+                <input type="password" id="confirm_password" name="confirm_password" required>
+            </fieldset>
 
             <input type="submit" value="Edit">
         </form>
         <div class="login-link">
-            <a href="..\Login\index.php">Login</a>
+            <a href="../Login/index.php">Login</a>
         </div>
     </div>
 </body>
