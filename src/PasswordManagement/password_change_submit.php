78825efe890dc0e57adc3a861e5f33064582e622
Augusto Madeira Splett <augusto.splett@gmail.com>
2024-04-04 09:13:17 -0400

    implemented the function on forget my password page

--- src/PasswordManagement/password_change_submit.php
+++ src/PasswordManagement/password_change_submit.php
@@ -1,16 +1,7 @@
 <?php
-$servername = "localhost"; 
-$username = "root"; 
-$password = "Joice123456*"; 
-$database = "kidsGames";
+require_once("../db/db_config.php");//get the connection configuration
 
-// Juan Pinero
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Error en la conexión a la base de datos: " . $conn->connect_error);
-}
+global $conn; //importing the default DB connection Configuration
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Obtener los datos del formulario
@@ -19,13 +10,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $lastName = $_POST['last_name'];
     $newPassword = $_POST['new_password'];
 
-    $sql = "SELECT * FROM usuarios WHERE username = '$username'";
-    $result = $conn->query($sql);
+    //query that verify if the user exist and retrive the registration ID
+    $sql = "SELECT  `fName`, `lName`, `registrationOrder`  FROM player WHERE `userName` = '$username'";
 
+    $result = mysqli_query($conn, $sql);
+
+    // Check if query was successful
+    if (!$result) {
+        die('Query failed: ' . mysqli_error($conn));
+    }
+    //get the values from db to check if we will allow the user to change the password
     if ($result->num_rows > 0) {
-        echo "User exists. Proceed with password change.";
-    } else {
-        echo "User not found. Unable to change password.";
+        while ($row = mysqli_fetch_assoc($result)) {
+            $db_registrationOrder = $row['registrationOrder'];
+            $db_fName = $row['fName'];
+            $db_lName = $row['lName'];
+        }
+    }else{
+        //user not found we will not do anything
+        return "User not found";
+    }
+
+    if(!($db_fName == $firstName)){
+        //dont allow to change the first name is different
+        return "Wrong First Name";
+    }
+
+    if(!($db_lName == $lastName)){
+        //dont allow to change the last name is different
+        return "Wrong Last Name";
+    }
+
+    //if the user reach this point, we will change the password
+    $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+    $updatePasswordQuery = "UPDATE authenticator SET `passCode`='$hashedPassword' where `registrationOrder`='$db_registrationOrder'";
+
+    $updateResult = mysqli_query($conn, $updatePasswordQuery);
+
+    // Check if query was successful
+    if (!$updateResult) {
+        die('Query failed: ' . mysqli_error($conn));
+    }else{
+        return "success!";
     }
 }
-?>
+
