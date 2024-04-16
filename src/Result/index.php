dcbc2e3a15fa9cf7a6284de22e493664d472c073
Augusto Madeira Splett <augusto.splett@gmail.com>
2024-04-15 21:46:56 -0400

    Adjusted style and add link to results page on the results page

--- src/Result/index.php
+++ src/Result/index.php
@@ -45,5 +45,11 @@
             </tr>
         </table>
     </div>
+    <div class="container">
+        <div>
+            <a href="../../index.php"> Home </a>
+            <a href="../Result/index.php"> Results </a>
+        </div>
+    </div>
 </body>
 </html>
