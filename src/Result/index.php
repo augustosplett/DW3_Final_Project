41a6318e7b11adfb3ef2a95ab757f9d2c631ef5e
Augusto Madeira Splett <augusto.splett@gmail.com>
2024-04-15 23:19:19 -0400

    Adjusted Style and Add the footer on the Result page

--- src/Result/index.php
+++ src/Result/index.php
@@ -31,25 +31,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KidsGame Ranking</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    
 </head>
 <body>
-    <div class="container">
-        <table>
-            <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Result</th>
-                <th scope="col">Lives Used</th>
-            </tr>
-            <tr>
-                <?php echo loadResultsTable(); ?>
-            </tr>
-        </table>
-    </div>
-    <div class="container">
-        <div>
-            <a href="../../index.php"> Home </a>
+    <div class="my-container">
+        <div class="glass-container">
+            <table>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Result</th>
+                    <th scope="col">Lives Used</th>
+                </tr>
+                <tr>
+                    <?php echo loadResultsTable(); ?>
+                </tr>
+            </table>
+            <br>
+                <a class="btn btn-success button-a" href="../../index.php"> Home </a>
+
         </div>
     </div>
+ 
+
     <footer class="footer">
         <ul>
             <li>College LaSalle</li>
