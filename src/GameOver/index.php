ec7f09a73917cac12bca3835067afa4e0c0ea6f1
Augusto Madeira Splett <augusto.splett@gmail.com>
2024-04-15 23:12:10 -0400

    Adjusted Style and Add the footer on the GameOver page

--- src/GameOver/index.php
+++ src/GameOver/index.php
@@ -5,14 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Game Over</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    
 </head>
 <body>
-    <div class="container">
-        <h1>Game Over</h1>
-        <div>
-            <a href="../../index.php"> Home </a>
-            <a href="../Result/index.php"> Results </a>
+    <div class="my-container">
+        <div class="container">
+            <h1>Game Over</h1>
+            <div>
+                <a class="btn btn-success button-a" href="../../index.php"> Home </a>
+                <a class="btn btn-success button-a" href="../Result/index.php"> Results </a>
+            </div>
         </div>
     </div>
+    <footer class="footer">
+        <ul>
+            <li>College LaSalle</li>
+            <li>DW3 Course</li>
+            <li>2024</li>
+            <li>Augusto Madeira Splett - Faiqa Faiqa - Juan Manuel Pinero Delgadillo - Rony Raug - Yasmeen Al Dali</li>
+        </ul>
+    </footer>
 </body>
 </html>
